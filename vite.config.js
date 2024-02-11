11c5fea48bf090778f32371781ea9e78eca2c144
Daniel Sales <danisales.es@gmail.com>
2024-02-11 23:29:54 +0100

    fix: vite building

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,47 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { glob } from "glob";
+import { extname, relative } from "node:path";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
+
+const inputSourceFiles = Object.fromEntries(
+  glob.sync("src/**/*.{ts,tsx}").map(file => {
+    const ext = extname(file);
+    const isCodeFile = ext === ".ts" || ext === ".tsx";
+    // The name of the entry point
+    // src/nested/foo.ts becomes nested/foo
+    const relativePath = relative("src", file.slice(0, file.length - (isCodeFile ? ext.length : 0)));
+
+    return [
+      relativePath,
+      // The absolute path to the entry file
+      // src/nested/foo.ts becomes /project/src/nested/foo.ts
+      file,
+    ];} ),
+);
 
 export default defineConfig( {
   build: {
     lib: {
-      entry: resolve(__dirname, "src/index.ts"), // Por si fuera tsx, para parsearlo y se pueda importar como index
-      name: "dauikit",
-
-      fileName: "index",
+      entry: inputSourceFiles,
+      formats: ["es"],
+      name: "index",
     },
     rollupOptions: {
-      external: ["react"],
+      input: inputSourceFiles,
+      external: ["react", "react/jsx-runtime"],
     },
+    output: {
+      treeshake: true,
+      entryFileNames: "[name].js",
+      assetFileNames: "assets/[name].[ext]",
+    },
+    minify: true,
     outDir: "build",
   },
-  plugins: [react()],
+  plugins: [react(), libInjectCss(), dts( {
+    include: ["src"],
+  } )],
 } );
